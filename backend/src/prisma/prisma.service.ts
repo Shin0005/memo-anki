127270d3351e62d826d34e65c38e927a7a5ef685
Shin0005 <kuroppen123@gmail.com>
2026-03-12 22:31:30 +0900

    fix: 型情報を追加し、any型を回避

--- backend/src/prisma/prisma.service.ts
+++ backend/src/prisma/prisma.service.ts
@@ -1,9 +1,10 @@
 import { Injectable, OnModuleInit } from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
 
+// any型を回避
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit {
-  async onModuleInit(): Promise<void> {
+  async onModuleInit() {
     await this.$connect();
   }
 }
