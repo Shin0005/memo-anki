636b172247fd840739b90dc4eb2a700e41e7ab95
Shin0005 <kuroppen123@gmail.com>
2026-03-16 19:21:17 +0900

    fix: userに型定義の追加とcreateの引数をDTOに変更

--- backend/src/user/user.repository.ts
+++ backend/src/user/user.repository.ts
@@ -1,23 +1,24 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from '.././prisma/prisma.service';
+import { Prisma, User } from '@prisma/client';
 
 @Injectable()
 export class UserRepository {
   constructor(private prismaService: PrismaService) {}
-  // asuncなしでも動くが、prismaがpromiseを返すので明示しておく。
-  async create(data: {
-    username: string;
-    passwordHash: string;
-    email?: string;
-  }) {
+  // asyncなしでも動くが、prismaがpromiseを返すので明示しておく。
+  async create(data: Prisma.UserCreateInput): Promise<User> {
     return this.prismaService.user.create({ data });
   }
 
-  async findByUsername(username: string) {
-    return this.prismaService.user.findUnique({ where: { username } });
+  async findByUsername(username: string): Promise<User | null> {
+    return this.prismaService.user.findUnique({
+      where: { username },
+    });
   }
 
-  async findByEmail(email: string) {
-    return this.prismaService.user.findUnique({ where: { email } });
+  async findByEmail(email: string): Promise<User | null> {
+    return this.prismaService.user.findUnique({
+      where: { email },
+    });
   }
 }
