3824bfa33e99d3db2b281941308b411a0d53f3b4
Shin0005 <kuroppen123@gmail.com>
2026-03-16 19:29:21 +0900

    feat: controller登録用のリクエストを作成

---
+++ backend/src/auth/dto/register.request.ts
@@ -0,0 +1,23 @@
+import {
+  IsString,
+  IsOptional,
+  IsEmail,
+  MinLength,
+  MaxLength,
+} from 'class-validator';
+
+export class RegisterRequest {
+  @IsString()
+  @MaxLength(30)
+  username: string;
+
+  @IsString()
+  @MinLength(8)
+  @MaxLength(64)
+  password: string;
+
+  @IsOptional()
+  @IsEmail()
+  @MaxLength(255)
+  email?: string;
+}
