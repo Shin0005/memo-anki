eb582056849bf7fe9bf4a7dfba4d2781bcad3f03
Shin0005 <kuroppen123@gmail.com>
2026-03-20 10:27:11 +0900

    fix: @NotBlankのデバックのしやすさを向上

--- backend/src/common/decorators/not-blank.decorator.ts
+++ backend/src/common/decorators/not-blank.decorator.ts
@@ -1,33 +1,24 @@
 import { applyDecorators } from '@nestjs/common';
-import {
-  IsString,
-  IsDefined,
-  MinLength,
-  ValidationOptions,
-} from 'class-validator';
+import { IsString, IsDefined, MinLength } from 'class-validator';
 import { Transform } from 'class-transformer';
 
 /**
- * Javaの@NotBlankに相当するデコレータ
+ * Javaの@NotBlankと@Trim()を合わせたようなデコレータ
  *
  * string かつ !null かつ !undefined かつ !"" かつ !" "
  * @param message 表示したいメッセージ
+ * @return Trimされた値
  */
 export function NotBlank(message?: string) {
-  // 各デコレータ共通のoptionを定義
-  const options: ValidationOptions = {
-    message: message || '$property は入力が必須です',
-  };
-
   return applyDecorators(
-    IsDefined(options),
-    IsString(options),
+    IsDefined({ message: message || '$property は必須です' }),
+    IsString({ message: message || '$property には文字列を入力してください' }),
 
     // 値をトリミング " " → "" （戻り値unknownでlintをパス）
     Transform(({ value }): unknown =>
       typeof value === 'string' ? value.trim() : value,
     ),
     // 空文字（""）でないことを保証
-    MinLength(1, options),
+    MinLength(1, { message: message || '$property を入力してください' }),
   );
 }
