df2b5068c9dc7d9a43ec3c7ecb7fb643b83ceaf8
Shin0005 <kuroppen123@gmail.com>
2026-03-21 21:29:46 +0900

    feat: バリデーション例外の実装

--- backend/src/common/filters/global.exception.filter.ts
+++ backend/src/common/filters/global.exception.filter.ts
@@ -3,19 +3,41 @@ import {
   Catch,
   ExceptionFilter,
   HttpException,
+  HttpStatus,
   Logger,
 } from '@nestjs/common';
 import { Request, Response } from 'express';
+import { ValidationFailedException } from '../exceptions/application.exceptions';
 
 @Catch()
 export class GlobalExceptionFilter implements ExceptionFilter {
+  // （時間があればWinstonやPinoで出力したい）
   private readonly logger = new Logger('ExceptionFilter');
 
   catch(exception: unknown, host: ArgumentsHost) {
     const ctx = host.switchToHttp();
-    // 自分が設定するレスポンス
     const response: Response = ctx.getResponse<Response>();
     const request = ctx.getRequest<Request>();
+
+    /*
+      validation例外（pipeでthrow）
+    */
+    if (exception instanceof ValidationFailedException) {
+      this.logger.warn(`${exception.message} - Path: ${request.url}`);
+
+      // Json例） "errors":[{password: '8文字以上で入力してください'}, ... ,{...}]
+      const details = exception.validationErrors.map((err) => ({
+        field: err.property,
+        message: Object.values(err.constraints || {})[0],
+      }));
+
+      return response.status(HttpStatus.BAD_REQUEST).json({
+        statusCode: HttpStatus.BAD_REQUEST,
+        message: exception.message,
+        errors: details,
+        timestamp: new Date().toISOString(),
+        path: request.url,
+      });
 
     // Http系の汎用例外（DomainExも含む）
     if (exception instanceof HttpException) {
