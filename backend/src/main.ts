1488cf5e0467315cd8d88cac08c5376e37f6a947
Shin0005 <kuroppen123@gmail.com>
2026-03-21 13:04:53 +0900

    feat: ドメイン例外とhttpフィルタの作成

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,12 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { customValidationPipe } from './common/pipes/validation.pipe';
+import { GlobalExceptionFilter } from './common/filters/global.exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   // バリデーション
   app.useGlobalPipes(customValidationPipe);
+
+  //filter
+  app.useGlobalFilters(new GlobalExceptionFilter());
 
   // CORS
   app.enableCors({
