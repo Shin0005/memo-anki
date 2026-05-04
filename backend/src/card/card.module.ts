d2e7e60603dd1c90e625d3b46027461acd452b93
Shin0005 <kuroppen123@gmail.com>
2026-05-04 20:23:46 +0900

    feat: ReviewServiceを実装

--- backend/src/card/card.module.ts
+++ backend/src/card/card.module.ts
@@ -4,12 +4,14 @@ import { CardController } from './card.controller';
 import { PrismaModule } from '../prisma/prisma.module';
 import { CardRepository } from './card.repository';
 import { DeckModule } from '../deck/deck.module';
+import { ReviewService } from './review.service';
 
 @Module({
   imports: [PrismaModule, DeckModule],
   controllers: [CardController],
   providers: [
     CardService,
+    ReviewService,
     {
       provide: 'ICardRepository', // DI用のtoken
       useClass: CardRepository, // 実装
